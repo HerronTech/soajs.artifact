9bd24d1ee186c322ed2f63343bebff481b5e8daa
antoinehage <me@antoinehage.com>
2016-04-20 19:23:39 -0400

    blank structure

---
+++ config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+    serviceName: "soajs.artifact",
+    serviceVersion: 1,
+    servicePort: 4999,
+    serviceGroup: "SOAJS artifact",
+    extKeyRequired: false,
+    type: "service",
+    prerequisites: {
+        cpu: '',
+        memory: ''
+    },
+    "errors": {},
+    "schema": {}
+};
---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+var soajs = require('soajs');
+var config = require('./config.js');
+
+var service = new soajs.server.service(config);
+
+service.init(function () {
+    
+ 
+});
